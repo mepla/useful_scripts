ff2182f1d0ad14e4ed7cbfbdf3f47489c0661c9a
Soheil Nasirian (Mepla) <s.nasirian@alohi.com>
2017-11-01 10:42:12 +0330

    updated bash arg script

--- bash/templates/argument_parsing_template.sh
+++ bash/templates/argument_parsing_template.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#READ: https://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash
+
 POSITIONAL=()
 while [[ $# -gt 0 ]]
 do
@@ -22,3 +24,41 @@ case $key in
 esac
 done
 set -- "${POSITIONAL[@]}"
+
+
+
+
+############################ -arg=x
+
+for i in "$@"
+do
+case $i in
+    -e=*|--extension=*)
+    EXTENSION="${i#*=}"
+    shift
+    ;;
+    -s=*|--searchpath=*)
+    SEARCHPATH="${i#*=}"
+    shift
+    ;;
+    -l=*|--lib=*)
+    LIBPATH="${i#*=}"
+    shift
+    ;;
+    --default)
+    DEFAULT=YES
+    shift
+    ;;
+    *)
+          # unknown option
+    ;;
+esac
+done
+echo "FILE EXTENSION  = ${EXTENSION}"
+echo "SEARCH PATH     = ${SEARCHPATH}"
+echo "LIBRARY PATH    = ${LIBPATH}"
+echo "Number files in SEARCH PATH with EXTENSION:" $(ls -1 "${SEARCHPATH}"/*."${EXTENSION}" | wc -l)
+if [[ -n $1 ]]; then
+    echo "Last line of file specified as non-opt/last argument:"
+    tail -1 $1
+fi
