acdc74fa667337ab5d4a4be7b8f30801f1464d16
Mepla <soheil.nasirian@gmail.com>
2025-03-25 18:46:25 +0100

    Update skat_calc.sh

--- bash/skatteverket/skat_calc.sh
+++ bash/skatteverket/skat_calc.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-for salary in $(seq 100000 1000 200000);
+for salary in $(seq 1000 1000 200000);
 do
-    curl_out=$(curl -s 'https://www7.skatteverket.se/portal-wapi/open/skatteberakning/v1/api/skattetabell/2024/beraknaSkatteavdrag' -H 'Content-Type: application/json' --data-raw "{\"skattesats\":32,\"inkomst\":$salary,\"fodelsear\":1990,\"typ\":\"L\"}")
+    curl_out=$(curl -s 'https://www7.skatteverket.se/portal-wapi/open/skatteberakning/v1/api/skattetabell/2025/beraknaSkatteavdrag' -H 'Content-Type: application/json' --data-raw "{\"skattesats\":32,\"inkomst\":$salary,\"fodelsear\":1990,\"typ\":\"L\"}")
     echo $salary, $(echo $curl_out | jq .lonefterskatt)
 done
 
